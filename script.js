5e34e72e9916dc78d742825b8cea3bccde7e7cf3
prashant3825 <67409568+prashant3825@users.noreply.github.com>
2020-06-25 13:17:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter Name");
+ var b=prompt("Enter password");
+  	if(a== "priya"&&b=="7348842604")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
